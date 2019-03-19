962d4c14fe78b6723b4337e59d8be43b00f07bc1
Yong Jin, Cho <drajin.cho@bosornd.com>
2019-03-19 09:48:59 +0900

    새로운 과제(Example4)를 생성한다.

---
+++ Example4/Example4.cpp
@@ -0,0 +1,14 @@
+﻿#include <CoalaMOD.h>
+#pragma comment(lib, "CoalaMOD.lib")
+
+int main()
+{
+	int px, py, pz;
+	getPlayerLocation(&px, &py, &pz);				// 현재 플레이어의 위치를 획득한다.
+
+	int x = px + 1, y, z = pz + 1;
+	for (y = 1; getBlockType(x, y, z) != BLOCK_AIR; y++);
+
+	BlockID stone = createBlock(BLOCK_STONE);		// STONE 블록을 생성한다.
+	locateBlock(stone, x, y, z);
+}
